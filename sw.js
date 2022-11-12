e1dabe425c441dcc298d1fdfd0f2200a2fd5c831
007Satvik <114695944+007Satvik@users.noreply.github.com>
2022-11-12 12:46:45 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -42,22 +42,24 @@ self.addEventListener('fetch', function (event) {
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
    // Open the cache
-   if (event.request.mode === 'navigate') {
-    // Open the cache
-    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-      // Go to the network first
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    // Go to the cache first
+    return cache.match(event.request).then((cachedResponse) => {
+      // Return a cached response if we have one
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // Otherwise, hit the network
       return fetch(event.request).then((fetchedResponse) => {
+        // Add the network response to the cache for later visits
         cache.put(event.request, fetchedResponse.clone());
 
+        // Return the network response
         return fetchedResponse;
-      }).catch(() => {
-        // If the network is unavailable, get
-        return cache.match(event.request.url);
       });
-    }));
-  } else {
-    return;
-  }
+    });
+  }));
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
