1353064269d25dde3df5294aea2e4ffac18afae2
007Satvik <114695944+007Satvik@users.noreply.github.com>
2022-11-11 23:03:41 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,18 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+
+  if("serviceWorker" in navigator ){
+    window.addEventListener('load', async event1 => {
+      try {
+        const registration = await navigator.serviceWorker.register("./sw.js", {scope:'./' }).then((registration) =>
+        console.log("Success"));
+      }
+      catch (error){
+        console.log('Failed');
+      }
+    } );
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -68,15 +80,40 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  let locrecipe;
+  if(window.localStorage.length != 0)
+   return locrecipe = JSON.parse(window.localStorage.getItem('recipes'));
+  //
+  //alert(locrecipe);
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  var locArray = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  var promise = new Promise(async (resolve, reject) => {
+    for(let i = 0; i < RECIPE_URLS.length; i++){
+      try{
+        const temp = await fetch(RECIPE_URLS[i]);
+        const rec = await temp.json();
+        console.log(rec);
+        locArray.push(rec);
+        if(i == RECIPE_URLS.length - 1){
+            saveRecipesToStorage(locArray);
+            resolve(locArray);
+        }
+      }
+      catch (error){
+          console.log(error);
+          reject(error);
+      }
+    }
+  });
+  return locArray
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
